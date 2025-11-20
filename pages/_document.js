ede579598c2fe707dafc3e38816e879ec6d5d6b0
makhlouf1102 <makhlouf.hennine.1@ens.etsmtl.ca>
2025-11-19 23:47:26 -0500

    Setup DaisyUI and TailwindCSS with theme toggle

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="light">
       <Head>
-        <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body>
         <Main />
